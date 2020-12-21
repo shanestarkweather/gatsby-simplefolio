f36004d7b83f4e6188a67028cadaf09344bdfc78
shanestarkweather <shane@shanestarkweather.com>
2020-12-21 00:40:36 -0800

    Ready to deploy

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,58 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Shane Starkweather | Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: `Hi. I'm`,
+  name: 'Shane Starkweather.',
+  subtitle: `I'm a full stack developer.`,
+  cta: 'Learn More',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: `I am a full stack software engineer. I have a background in which the only way to survive was to figure things out with the resources at hand, and not always necessarily with any budget. That has created in me a strong sense of how to succeed despite not always having tools, but the ability and desire to find and use the needed tools. That background, in management in my family’s business, made me aware of all the working pieces of teams and business affording me a more complete picture in many situations and challenges.`,
+  paragraphTwo: ' ',
+  paragraphThree: ' ',
+  resume: 'https://drive.google.com/file/d/1ET0riA3-MHdD8a5fB-CdeQuHD_Kp3Ez3/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'AllTheFeels.png',
+    title: 'All The Feels - Full Stack Group Project',
+    info:
+      'This was a group project I completed with three other people as part of the software engineering immersive I completed at General Assembly',
+    info2: 'React, Node, Express, MongoDB, ',
+    url: 'https://all-the-feels-fe.herokuapp.com/',
+    repo: 'https://github.com/Kid-Then-Kid-Now/front-end', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'WhatsShakin.png',
+    title: `What's Shakin'?`,
+    info: 'This React app connects to a U.S. Geological Survey API to gather data on earthquakes.',
+    info2: 'React, CSS',
+    url: 'https://whats-shakin.herokuapp.com/',
+    repo: 'https://github.com/shanestarkweather/earthquake', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'DroneLog.png',
+    title: 'Drone Log',
+    info:
+      'A full stack app I developed myself with user authorization to create and store flight logs for drone pilots.',
+    info2: 'React, Django, Python, PostgreSQL',
+    url: 'https://drone-log.herokuapp.com/',
+    repo: 'https://github.com/shanestarkweather/droneappfe', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +61,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'shane@shanestarkweather.com',
 };
 
 // FOOTER DATA
@@ -78,17 +80,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/shanestarkweather/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/shanestarkweather',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
